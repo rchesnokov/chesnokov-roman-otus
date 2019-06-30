c4ed0deaa7e527863b4aa6a988333858d15d2d7f
Roman Chesnokov <roman.chesnokov@gmail.com>
2019-07-01 00:36:20 +0300

    javascript-1

---
+++ javascript/javascript-1/utils.js
@@ -0,0 +1,24 @@
+const isEveryNumber = (args) => args.every(n => typeof n === 'number');
+
+const checkArgsLength = (args) => {
+  if (args.length === 0) {
+    console.error('You must provide arguments!');
+    return process.exit(-1);
+  }
+
+  return true;
+};
+
+const checkArgsType = (args) => {
+  if (!isEveryNumber(args)) {
+    console.error('You must provide numbers!');
+    return process.exit(-1);
+  }
+
+  return true;
+};
+
+module.exports = {
+  checkArgsLength,
+  checkArgsType,
+};
